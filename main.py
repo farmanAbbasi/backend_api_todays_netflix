cf77be96c4cf1f7a91846dc48619c468abbed375
Mohammad Farman Abbasi <32016615+farmanAbbasi@users.noreply.github.com>
2021-01-21 21:14:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,55 @@ def getMovieUrl():
                   "image_url":img_url}
             finalArray.append(data)
 
-    return finalArray                 
+    return finalArray      
+
+def getNewsUrl(language):
+    url_hindi = "https://timesofindia.indiatimes.com/entertainment/hindi/bollywood/news"
+    url_english="https://timesofindia.indiatimes.com/entertainment/english/hollywood/news"
+    if language=="en":
+        url=url_english
+    else:
+        url=url_hindi
+    
+    html_content = requests.get(url).text
+    soup = BeautifulSoup(html_content, "html.parser")
+    special_divs = soup.find("div", {"id": "mainlisting"})
+    special_anchors=special_divs.find_all("a", href = True)
+    #print(special_anchors)
+    finalData=[]
+    for text in special_anchors:
+       
+       link="https://timesofindia.indiatimes.com"+text['href']
+       
+       msg=text.text
+       for img in text('img'):
+           image_src=img['src']
+           image_src=image_src.replace("width-134,height-99","width-500,height-350")
+           
+       returnData={
+            "link":link,
+            "msg":msg,
+            "image_src":image_src
+           }
+       finalData.append(returnData)
+    finalData.pop()
+
+    finalData2=[]
+    for f in finalData:
+        print(f)
+        if f["msg"]=="":
+            continue
+        else:
+            finalData2.append(f)
+    return finalData2
+
+           
+
+@app.route('/hollyBollyToday', methods=['GET'])
+def loadDataHolly():
+    language=request.args.get('language')
+    finalData=getNewsUrl(language)
+    return json.dumps({"data": finalData})  
             
 
 
